86fbe9c5dac54bd4e6fd28af68ce0e5d7f7287e8
Michael Miles <boat081062@gmail.com>
2016-05-09 06:09:13 -0500

    Lambda will take some practice much like ternary did.

---
+++ LambdaExpressions/LambdaExpressions/Book.cs
@@ -0,0 +1,8 @@
+namespace LambdaExpressions
+{
+    class Book
+    {
+        public int Price { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main()
+        {
+            //args => expression
+
+            //() => ...         (No arguments)
+            //x => ...          (One argument)
+            //(x, y, z) => ...  (More than one argument)
+
+            Func<int, int> square = number => number*number;
+            Console.WriteLine($"{square(5)}");
+
+            const int factor = 5;
+            Func<int, int> multiplier = n => n*factor;
+            Console.WriteLine($"{multiplier(10)}");
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        //static bool IsCheaperThan10Dollars(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
+
+        //Normal method
+/*      same as the lambda expression above
+        static int Square(int number)
+        {
+            return number*number;
+        }
+*/
+    }
+}
