a10128f3b62701c6d822f8e75bba018ed94b2408
Michael Miles <boat081062@gmail.com>
2016-05-10 12:40:05 -0500

    Extention Methods - much easier than I remember

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ExtensionMethods
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
 {
     class Program
     {
         static void Main()
         {
+            string post = "This is supposed to be a very long blog post blah blah blah ...";
+            //var shortenedPost = post.Shorten(5);
 
+            IEnumerable<int> numbers = new List<int> {1, 5, 3, 10, 2, 18};
+            var max = numbers.Max();
+
+            Console.WriteLine($"Shortened Post: \"{post.Shorten(5)}...\"");
+            Console.WriteLine($"Maximum in numbers: {max}");
         }
     }
 }
