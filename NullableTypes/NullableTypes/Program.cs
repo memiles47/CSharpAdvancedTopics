370bb1f4e162b8fc0eb69d00b6cb6cc4ad4d0d6d
Michael E Miles <michael@miles-me.net>
2016-05-11 03:05:14 -0500

    More practice with nullable types

--- NullableTypes/NullableTypes/Program.cs
+++ NullableTypes/NullableTypes/Program.cs
@@ -11,9 +11,19 @@ namespace NullableTypes
             Console.WriteLine($"GetValueOrDefault(): {date.GetValueOrDefault()}");
             Console.WriteLine($"HasValue: {date.HasValue}");
 
-            //This will not work but I will find a solution with nullcoalecing
+            //This will not work but I will find a solution with nullcoalecing (I did)
             //Console.WriteLine($"Value: {date.Value}");
-            Console.WriteLine($"Value: {date.GetValueOrDefault()}");
+            //Console.WriteLine($"Value: {date.GetValueOrDefault()}");
+            Console.WriteLine($"Value: {(date == null ? "Null": DateTime.Now.ToString())}");
+            Console.WriteLine();
+
+            DateTime? date1 = null;
+
+            //will not work. You can't put a nullable into a non nullable practice.
+            //DateTime date2 = date1;
+            DateTime date2 = date1.GetValueOrDefault();
+            Console.WriteLine($"Value: {date2.DayOfWeek}");
+
         }
     }
 }
