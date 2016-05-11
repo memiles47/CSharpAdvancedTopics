afab5308936d3247f014f955d45f9676fde2e7cd
Michael E Miles <michael@miles-me.net>
2016-05-10 19:26:52 -0500

    Nullable Types project

---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime? date = null;
+
+            Console.WriteLine($"GetValueOrDefault(): {date.GetValueOrDefault()}");
+            Console.WriteLine($"HasValue: {date.HasValue}");
+
+            //This will not work but I will find a solution with nullcoalecing
+            //Console.WriteLine($"Value: {date.Value}");
+            Console.WriteLine($"Value: {date.GetValueOrDefault()}");
+        }
+    }
+}
